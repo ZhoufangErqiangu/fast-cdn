846294c842430d088e66f3e275aa9e0d052cd2ec
alex <liuhlightning@qq.com>
2023-06-24 10:21:11 +0800

    use ui

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { FastCrud } from "@fast-crud/fast-crud";
+import { useUi } from "@fast-crud/ui-interface";
 import FsUiElement from "@fast-crud/ui-element";
 import ElementPlus from "element-plus";
 import { createApp } from "vue";
@@ -6,9 +6,11 @@ import App from "./App.vue";
 import { router } from "./router";
 import "./style.css";
 
-createApp(App)
-  .use(router)
-  .use(ElementPlus)
-  .use(FsUiElement)
-  .use(FastCrud)
-  .mount("#app");
+const app = createApp(App);
+app.use(router).use(ElementPlus);
+const e = FsUiElement.install(app);
+const u = useUi();
+u.set(e);
+
+import { FastCrud } from "@fast-crud/fast-crud";
+app.use(FastCrud, { ui: u.ui }).mount("#app");
