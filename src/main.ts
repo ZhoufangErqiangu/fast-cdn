1d3e0b1b4b9f19ce37ff623c981f95815c49f20b
Alex Liu <45325262+ZhoufangErqiangu@users.noreply.github.com>
2023-07-23 10:39:11 +0800

    Revert "Dev (#1)"
    This reverts commit e4de7620d625006a33d5d798f08e4d6fe9a5692a.

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,9 @@
-import { useUi } from "@fast-crud/ui-interface";
-import FsUiElement from "@fast-crud/ui-element";
+import { FastCrud } from "@fast-crud/fast-crud";
+import ui from "@fast-crud/ui-element";
 import ElementPlus from "element-plus";
 import { createApp } from "vue";
 import App from "./App.vue";
-import { router } from "./router";
+import router from "./router";
 import "./style.css";
 
-const app = createApp(App);
-app.use(router).use(ElementPlus);
-const e = FsUiElement.install(app);
-const u = useUi();
-u.set(e);
-
-import { FastCrud } from "@fast-crud/fast-crud";
-app.use(FastCrud, { ui: u.ui }).mount("#app");
+createApp(App).use(router).use(ElementPlus).use(ui).use(FastCrud).mount("#app");
